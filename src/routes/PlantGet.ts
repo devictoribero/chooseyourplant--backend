7b2bf0bfbf1dd4bf7d9dcfeb40a28371070024d5
victor ribero <victor.ribero@schibsted.com>
2020-10-10 21:11:35 +0200

    Change name

--- src/routes/PlantGet.ts
+++ src/routes/PlantGet.ts
@@ -1,16 +1,16 @@
 import { Router, Request, Response } from "express";
 import { PlantFinderById } from "../contexts/Plants/Application/Find/PlantFinderById";
 import { MongoPlantRepository } from "../contexts/Plants/Infrastructure/MongoPlantRepository";
-import { PlantsGetByIdController } from "../controllers/Plants/PlantsGetByIdController";
+import { PlantGetByIdController } from "../controllers/Plants/PlantsGetByIdController";
 
 const router = Router();
 // Dependencies for creating a plant
 const mongoPlantRepository = new MongoPlantRepository();
 const plantFinder = new PlantFinderById(mongoPlantRepository);
-const plantsGetByIdController = new PlantsGetByIdController(plantFinder);
+const plantGetByIdController = new PlantGetByIdController(plantFinder);
 
 router.get("/plants/:id", (req: Request, res: Response) =>
-  plantsGetByIdController.run(req, res)
+  plantGetByIdController.run(req, res)
 );
 
 export default router;
