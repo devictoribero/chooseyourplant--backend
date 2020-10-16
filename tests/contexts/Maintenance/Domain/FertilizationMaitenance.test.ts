2a5e93074d9eff9aefd27f1eee17c9579672b7af
victor ribero <victor.ribero@schibsted.com>
2020-10-16 17:00:44 +0200

    Added unit tests for Watering and Maintance

--- tests/contexts/Maintenance/Domain/FertilizationMaitenance.test.ts
+++ tests/contexts/Maintenance/Domain/FertilizationMaitenance.test.ts
@@ -4,55 +4,78 @@ describe("FertilizationMaintenance Domain Model ----", () => {
   describe('FertilizationMaintenance is instanced correctly with a the most basic Fertilization maintenance information', () => {
     it(`
         GIVEN
-          frequencyInDays: 4
+          frequencyInDays: 7
         THEN
-          the Fertilization frequencyInDays must be 4
+          the Fertilization frequencyInDays must be 7
       `, () => {
       // GIVEN
-      const fertilizationMaintenance = new FertilizationMaintenance(4);
+      const fertilization = new FertilizationMaintenance(7);
       // THEN
-      expect(fertilizationMaintenance.getFrequencyInDays()).toEqual(4);
+      expect(fertilization.getFrequencyInDays()).toEqual(7);
     });
   })
 
   describe('FertilizationMaintenance is instanced correctly given a lastFertilizationDate', () => {
     it(`
         GIVEN
-          frequencyInDays: 4,
-          lastFertilizationDate: '2020-10-16T07:41:59.983Z'
+          frequencyInDays: 7
+          lastFertilizationDate: '2020-10-16T14:46:00.000Z'
         THEN
-          the lastFertilizationDate must be '2020-10-16T07:41:59.983Z'
-          the nextFertilizationDate must be the lastFertilizationDate + 4 days
-          the nextFertilizationDate must be '2020-10-20T07:41:59.983Z'
+          the lastFertilizationDate must be '2020-10-16T14:46:00.000Z'
+          the nextFertilizationDate must be the lastFertilizationDate + 7 days
+          the nextFertilizationDate must be '2020-10-23T14:46:00.000Z'
       `, () => {
-        // GIVEN
-      const fertilizationMaintenance = new FertilizationMaintenance(4, null, new Date('2020-10-16T07:41:59.983Z'))
+      // GIVEN
+      const fertilization = new FertilizationMaintenance(7, new Date('2020-10-16T14:46:00.000Z'), null)
       // THEN
-      expect(fertilizationMaintenance.getLastFertilizationDate()).toEqual(null)
-      expect(fertilizationMaintenance.getNextFertilizationDate()).toEqual(new Date('2020-10-16T07:41:59.983Z'))
+      expect(fertilization.getLastFertilizationDate())
+        .toEqual(new Date('2020-10-16T14:46:00.000Z'))
+      expect(fertilization.getNextFertilizationDate())
+        .toEqual(new Date('2020-10-23T14:46:00.000Z'))
+    });
+  })
+
+  describe('FertilizationMaintenance is instanced correctly given a nextFertilizationDate', () => {
+    it(`
+        GIVEN
+          frequencyInDays: 7
+          nextFertilizationDate: '2020-10-16T14:46:00.000Z'
+        THEN
+          the lastFertilizationDate must be NULL
+          the nextFertilizationDate must be the lastFertilizationDate + 4 days
+          the nextFertilizationDate must be '2020-10-16T14:46:00.000Z'
+      `, () => {
+      // GIVEN
+      const fertilization = new FertilizationMaintenance(7, null, new Date('2020-10-16T14:46:00.000Z'))
+      // THEN
+      expect(fertilization.getLastFertilizationDate()).toEqual(null)
+      expect(fertilization.getNextFertilizationDate())
+        .toEqual(new Date('2020-10-16T14:46:00.000Z'))
     });
   })
 
   describe('FertilizationMaintenance is instanced correctly given a complete Fertilization specification', () => {
     it(`
         GIVEN
-          frequencyInDays: 4,
-          lastFertilizationDate: '2020-10-16T07:41:59.983Z',
-          nextFertilizationDate: '2020-10-20T07:41:59.983Z'
+          frequencyInDays: 7
+          lastFertilizationDate: '2020-10-16T14:46:00.000Z',
+          nextFertilizationDate: '2020-10-23T14:46:00.000Z'
         THEN
-          the Fertilization frequencyInDays must be 4
-          the lastFertilizationDate must be '2020-10-16T07:41:59.983Z'
-          the nextFertilizationDate must be '2020-10-20T07:41:59.983Z'
+          the Fertilization frequencyInDays must be 7
+          the lastFertilizationDate must be '2020-10-16T14:46:00.000Z'
+          the nextFertilizationDate must be '2020-10-23T14:46:00.000Z'
       `, () => {
         // GIVEN
-        const fertilizationMaintenance = new FertilizationMaintenance(
+        const fertilization = new FertilizationMaintenance(
           4,
-          new Date("2020-10-16T07:41:59.983Z"),
-          new Date('2020-10-20T07:41:59.983Z'),
+          new Date("2020-10-16T14:46:00.000Z"),
+          new Date('2020-10-23T14:46:00.000Z'),
         )
         // THEN
-        expect(fertilizationMaintenance.getLastFertilizationDate()).toEqual(new Date('2020-10-16T07:41:59.983Z'))
-        expect(fertilizationMaintenance.getNextFertilizationDate()).toEqual(new Date('2020-10-20T07:41:59.983Z'))
+        expect(fertilization.getLastFertilizationDate())
+          .toEqual(new Date('2020-10-16T14:46:00.000Z'))
+        expect(fertilization.getNextFertilizationDate())
+          .toEqual(new Date('2020-10-23T14:46:00.000Z'))
       });
     })
 });
--- tests/contexts/Maintenance/Domain/WateringMaitenance.test.ts
+++ tests/contexts/Maintenance/Domain/WateringMaitenance.test.ts
@@ -1,5 +1,4 @@
 import { WateringMaintenance } from "../../../../src/PlantManager/Maintenance/Domain/WateringMaintenance";
-import dayjs from 'dayjs'
 
 describe("WateringMaintenance Domain Model ----", () => {
   describe('WateringMaintenance is instanced correctly with a the most basic watering maintenance information', () => {
@@ -8,11 +7,13 @@ describe("WateringMaintenance Domain Model ----", () => {
           frequencyInDays: 4
         THEN
           the watering frequencyInDays must be 4
+          the watering frequencyInDays must be 4
+          the watering frequencyInDays must be 4
       `, () => {
       // GIVEN
-      const wateringMaintenance = new WateringMaintenance(4);
+      const watering = new WateringMaintenance(4);
       // THEN
-      expect(wateringMaintenance.getFrequencyInDays()).toEqual(4);
+      expect(watering.getFrequencyInDays()).toEqual(4);
     });
   })
 
@@ -20,17 +21,36 @@ describe("WateringMaintenance Domain Model ----", () => {
     it(`
         GIVEN
           frequencyInDays: 4,
-          lastWateringDate: '2020-10-16T07:41:59.983Z'
+          lastWateringDate: '2020-10-16T14:46:00.000Z'
         THEN
-          the lastWateringDate must be '2020-10-16T07:41:59.983Z'
+          the lastWateringDate must be '2020-10-16T14:46:00.000Z'
           the nextWateringDate must be the lastWateringDate + 4 days
-          the nextWateringDate must be '2020-10-20T07:41:59.983Z'
+          the nextWateringDate must be '2020-10-20T14:46:00.000Z'
       `, () => {
-        // GIVEN
-      const wateringMaintenance = new WateringMaintenance(4, null, new Date('2020-10-16T07:41:59.983Z'))
+      // GIVEN
+      const watering = new WateringMaintenance(4, new Date('2020-10-16T14:46:00.000Z'), null)
       // THEN
-      expect(wateringMaintenance.getLastWateringDate()).toEqual(null)
-      expect(wateringMaintenance.getNextWateringDate()).toEqual(new Date('2020-10-16T07:41:59.983Z'))
+      expect(watering.getLastWateringDate()).toEqual(new Date('2020-10-16T14:46:00.000Z'))
+      expect(watering.getNextWateringDate()).toEqual(new Date('2020-10-20T14:46:00.000Z'))
+    });
+  })
+
+  describe('WateringMaintenance is instanced correctly given a nextWateringDate', () => {
+    it(`
+        GIVEN
+          frequencyInDays: 4,
+          nextWateringDate: '2020-10-16T14:46:00.000Z'
+        THEN
+          the lastWateringDate must be NULL
+          the nextWateringDate must be the lastWateringDate + 4 days
+          the nextWateringDate must be '2020-10-16T14:46:00.000Z'
+      `, () => {
+      // GIVEN
+      const watering = new WateringMaintenance(4, null, new Date('2020-10-16T14:46:00.000Z'))
+      // THEN
+      expect(watering.getLastWateringDate()).toEqual(null)
+      expect(watering.getNextWateringDate())
+        .toEqual(new Date('2020-10-16T14:46:00.000Z'))
     });
   })
 
@@ -38,22 +58,24 @@ describe("WateringMaintenance Domain Model ----", () => {
     it(`
         GIVEN
           frequencyInDays: 4,
-          lastWateringDate: '2020-10-16T07:41:59.983Z',
-          nextWateringDate: '2020-10-20T07:41:59.983Z'
+          lastWateringDate: '2020-10-16T14:46:00.000Z',
+          nextWateringDate: '2020-10-20T14:46:00.000Z'
         THEN
           the watering frequencyInDays must be 4
-          the lastWateringDate must be '2020-10-16T07:41:59.983Z'
-          the nextWateringDate must be '2020-10-20T07:41:59.983Z'
+          the lastWateringDate must be '2020-10-16T14:46:00.000Z'
+          the nextWateringDate must be '2020-10-20T14:46:00.000Z'
       `, () => {
         // GIVEN
-        const wateringMaintenance = new WateringMaintenance(
+        const watering = new WateringMaintenance(
           4,
-          new Date("2020-10-16T07:41:59.983Z"),
-          new Date('2020-10-20T07:41:59.983Z'),
+          new Date("2020-10-16T14:46:00.000Z"),
+          new Date('2020-10-20T14:46:00.000Z'),
         )
         // THEN
-        expect(wateringMaintenance.getLastWateringDate()).toEqual(new Date('2020-10-16T07:41:59.983Z'))
-        expect(wateringMaintenance.getNextWateringDate()).toEqual(new Date('2020-10-20T07:41:59.983Z'))
+        expect(watering.getLastWateringDate())
+          .toEqual(new Date('2020-10-16T14:46:00.000Z'))
+        expect(watering.getNextWateringDate())
+          .toEqual(new Date('2020-10-20T14:46:00.000Z'))
       });
     })
 });
